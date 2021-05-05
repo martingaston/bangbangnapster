072814fd65983c9f66f53b30de116344c4390631
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-05-05 21:02:52 +0100

    setup basic tcp echo server from python docs

---
+++ server/server.py
@@ -0,0 +1,16 @@
+import socketserver
+
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print(f"{self.client_address[0]} wrote:")
+        print(self.data)
+        self.request.sendall(self.data.upper())
+
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        server.serve_forever()
