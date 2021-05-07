66e1e92d97e8f9f24b0f172488701d9931eb78c2
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-05-07 21:33:52 +0100

    implement skipped test in test_index_server

--- tests/server/test_index_server.py
+++ tests/server/test_index_server.py
@@ -1,32 +1,31 @@
-import pytest
+from src.payload_parser import parse_payload
 
 
 class File:
-    def __init__(self, filename, md5, bytes, bitrate, frequency, time):
+    def __init__(self, filename, md5, size_in_bytes, bitrate, frequency, time):
         self.filename = filename
         self.md5 = md5
-        self.bytes = bytes
+        self.size_in_bytes = size_in_bytes
         self.bitrate = bitrate
         self.frequency = frequency
         self.time = time
 
     @classmethod
-    def from_bytes(cls, byte_input):
-        [filename, md5, bytes, bitrate, frequency, time] = byte_input.decode(
-            encoding="ascii"
-        ).split(" ")
+    def from_bytes(cls, byte_input: bytes):
+        parsed = parse_payload(byte_input)
+
+        [filename, md5, size_in_bytes, bitrate, frequency, time] = parsed
 
         return cls(
             filename=filename,
             md5=md5,
-            bytes=int(bytes),
+            size_in_bytes=int(size_in_bytes),
             bitrate=int(bitrate),
             frequency=int(frequency),
             time=int(time),
         )
 
 
-@pytest.mark.skip()
 def test_can_make_a_file_from_bytes():
     bytes = b'"generic band - generic song.mp3" b92870e0d41bc8e698cf2f0a1ddfeac7-443008 443332 128 44100 60'
 
@@ -34,7 +33,7 @@ def test_can_make_a_file_from_bytes():
 
     assert result.filename == "generic band - generic song.mp3"
     assert result.md5 == "b92870e0d41bc8e698cf2f0a1ddfeac7-443008"
-    assert result.bytes == 443332
+    assert result.size_in_bytes == 443332
     assert result.bitrate == 128
     assert result.frequency == 44100
     assert result.time == 60
