ab7a151503cc408d68577704ab8484aefa0ee740
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-05-07 21:29:35 +0100

    implement payload_parser

--- src/payload_parser.py
+++ src/payload_parser.py
@@ -2,4 +2,38 @@ from typing import List
 
 
 def parse_payload(data: bytes) -> List[str]:
-    return ["foo", "badpass", "6699", "nap v0.8", "3"]
+    parsed = []
+
+    in_string = False
+    last_char_was_end_of_string = False
+    current = []
+
+    # concatenating a space at the end means we finish by pushing the final item in current to the parsed list
+    for x in data.decode("ascii") + " ":
+        if last_char_was_end_of_string:
+            last_char_was_end_of_string = False
+            continue
+
+        if in_string and x == '"':
+            parsed.append("".join(current))
+            in_string = False
+            last_char_was_end_of_string = True
+            current = []
+            continue
+
+        if in_string:
+            current.append(x)
+            continue
+
+        if x == '"':
+            in_string = True
+            continue
+
+        if x == " ":
+            parsed.append("".join(current))
+            current = []
+            continue
+
+        current.append(x)
+
+    return parsed
