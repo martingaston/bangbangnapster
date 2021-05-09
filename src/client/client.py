baa83c6b381700040ed7855381dc241533a6b0e3
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-05-09 17:05:29 +0100

    unshare files when client exits

--- src/client/client.py
+++ src/client/client.py
@@ -1,11 +1,11 @@
 import socket
-from time import sleep
 
 HOST, PORT = "localhost", 5000
 # HOST, PORT = "ec2-52-56-46-175.eu-west-2.compute.amazonaws.com", 5000
 packet_data = b'\x1d\x00\x02\x00foo badpass 6699 "nap v0.8" 3'
 mp3_data = b'\x5d\x00\x64\x00"generic band - generic song.mp3" b92870e0d41bc8e698cf2f0a1ddfeac7-443008 443332 128 44100 60'
 search_request = b"\x07\x00\xC8\x00generic"
+unshare_all = b"\x00\x00\x6E\x00"
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,6 +32,7 @@ packet_data = sock.recv(int.from_bytes(packet_length, byteorder="little"))
 
 print(packet_data.decode("ascii"))
 
+sock.sendall(unshare_all)
 sock.close()
 
 print("Sent:     {}".format(packet_data))
--- src/server/server.py
+++ src/server/server.py
@@ -5,13 +5,14 @@ from src.server.user import User
 from src.server.file import File
 from src.packet import Packet
 from src.packet_type import PacketType
+from typing import Optional
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         while True:
             packet = self._read_packet()
-            if packet.packet_length == 0:
+            if packet is None:
                 break
 
             thread = threading.current_thread()
@@ -23,12 +24,15 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             if response is not None:
                 self.request.sendall(bytes(response))
 
-    def _read_packet(self) -> Packet:
+    def _read_packet(self) -> Optional[Packet]:
         packet_length = self.request.recv(2)
         packet_type = int.from_bytes(self.request.recv(2), byteorder="little")
         packet_data = self.request.recv(
             int.from_bytes(packet_length, byteorder="little")
         )
+
+        if packet_length == b"" and packet_type == 0 and packet_data == b"":
+            return None
 
         return Packet(PacketType(packet_type), packet_data.decode("ascii"))
 
@@ -42,6 +46,16 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             file = File.from_bytes(packet.data)
             index_server.add(file, self.user)
             return None
+
+        if (
+            packet.packet_type
+            == PacketType.UNSHARE_ALL_FILES_FROM_THE_SHARED_FILE_INDEX
+        ):
+            index_server = IndexServer()
+            removed = index_server.remove_all(self.user)
+            return Packet(
+                PacketType.UNSHARE_ALL_FILES_FROM_THE_SHARED_FILE_INDEX, str(removed)
+            )
 
         if packet.packet_type == PacketType.SEARCH_QUERY_REQUEST:
             index_server = IndexServer()
