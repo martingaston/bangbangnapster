c76d7c58e9b2ddb7c94573441f8bb324a5f9dfb3
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-05-09 21:08:02 +0100

    start to spike out peer server

--- src/client/client.py
+++ src/client/client.py
@@ -3,12 +3,20 @@ import socket
 from src.packet import Packet, read_packet
 from src.packet_type import PacketType
 from typing import Optional
+from time import sleep
+import threading
 
 # HOST, PORT = "ec2-52-56-46-175.eu-west-2.compute.amazonaws.com", 5000
 packet_data = b'\x1d\x00\x02\x00foo badpass 6699 "nap v0.8" 3'
 mp3_data = b'\x5d\x00\x64\x00"generic band - generic song.mp3" b92870e0d41bc8e698cf2f0a1ddfeac7-443008 443332 128 44100 60'
 search_request = b"\x07\x00\xC8\x00generic"
 unshare_all = b"\x00\x00\x6E\x00"
+
+
+def peer_server():
+    while True:
+        print("threads!")
+        sleep(2)
 
 
 class Client:
@@ -20,6 +28,9 @@ class Client:
         self.port = port
 
     def start(self):
+
+        x = threading.Thread(target=peer_server, daemon=True)
+        x.start()
         # Create a socket (SOCK_STREAM means a TCP socket)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect((self.host, self.port))
