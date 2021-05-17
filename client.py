0e95b6a5bee28bdaaf80606237f6d5af82b83f8c
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-05-17 13:54:22 +0100

    fix path to server (#4)

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from src.client.client import Client
 
+HOST, PORT = "ec2-52-56-46-175.eu-west-2.compute.amazonaws.com", 5000
 if __name__ == "__main__":
-    client = Client()
+    client = Client(host=HOST)
     client.start()
