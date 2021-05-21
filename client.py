34ce8ebaeec13e2087898f7a73caa17f39fdc45a
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-05-21 21:26:07 +0100

    Add server as a client (#6)
    * add a client server
    
    * add shared back into gitignore

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 from src.client.client import Client
 
-HOST, PORT = "ec2-52-56-46-175.eu-west-2.compute.amazonaws.com", 5000
+HOST, PORT = "ec2-52-56-46-175.eu-west-2.compute.amazonaws.com", 7777
 if __name__ == "__main__":
     client = Client(host=HOST)
     client.start()
